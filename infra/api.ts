8707f94833cc244e61dc3f17d2ec4748e06b1616
qYogi <bogdanviziru18@gmail.com>
2024-09-17 16:59:49 +0300

    Am creat un proiect simplu cu SST, Vite+React si TanStack Router

--- infra/api.ts
+++ infra/api.ts
@@ -1,7 +1,9 @@
-import { bucket } from "./storage";
-
-export const myApi = new sst.aws.Function("MyApi", {
-  url: true,
-  link: [bucket],
-  handler: "packages/functions/src/api.handler"
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: "./packages/functions/src/api.handler",
+      },
+    },
+  },
 });
