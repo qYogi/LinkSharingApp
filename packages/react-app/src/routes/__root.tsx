9b3569e89227417a01067593a81f1ea30c9524e2
qYogi <bogdanviziru18@gmail.com>
2024-09-18 13:40:44 +0300

    I've completley set up the app with TanStackRoueter and TanStackQuery, and Tailwind

--- packages/react-app/src/routes/__root.tsx
+++ packages/react-app/src/routes/__root.tsx
@@ -1,13 +1,19 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2"></div>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+      </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools />
     </>
   ),
 });
